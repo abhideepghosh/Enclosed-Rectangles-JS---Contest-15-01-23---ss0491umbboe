2962c77f3d73d4cee14cbd45ee98f16f07346066
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-15 19:21:56 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+//rec = {
+//	top: '25px',
+//	left: '96px',
+//	width: '64px',
+//	height: '96px',
+//      children: []
+//}
+
+function updateStructure(rec1,rec2){
+	//write your code
+}
+
+module.exports = updateStructure;
