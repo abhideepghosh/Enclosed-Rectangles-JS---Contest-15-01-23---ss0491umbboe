3bd5988a34a09c8f3c432c3157240e489679577c
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-01-15 19:24:36 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
 
-
-// ReactDOM.render(<App />, document.getElementById("root"));
